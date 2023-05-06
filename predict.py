66c01ec15903e19514b879ceaca5c5529fa08d61
Jacob Dawson <jacob.dawson@me.com>
2023-05-06 12:53:42 -0400

    output predictions to midi

--- predict.py
+++ predict.py
@@ -4,12 +4,16 @@
 
 import numpy as np
 import tensorflow as tf
+import pretty_midi
 from arch import *
 
 # this determines the length of the song made:
 numPredictions = 128
 
+predictionFile = "prediction.mid"
+
 # make model
+print("Retrieving model")
 model = attentionModel((timestepsPerBatch, 128))
 
 # and compile... this doesn't actually matter because we aren't training
@@ -23,9 +27,11 @@ model.compile(
 model.built = True
 
 # and finally load our weights
+print("Retrieving weights")
 model.load_weights(modelSaveLocation)
 
 # predict!
+print("Making predictions")
 predictions = []
 lastPrediction = np.zeros((timestepsPerBatch, 128), dtype=np.float16)
 for i in range(numPredictions):
@@ -34,6 +40,26 @@ for i in range(numPredictions):
     predictions.append(prediction)
     # print(f"lastPrediction shape: {lastPrediction.shape}, prediction shape: {prediction.shape}")
     lastPrediction = prediction
+
+print("Postprocessing predictions")
 predictions = np.concatenate(predictions, axis=0)
 predictions = predictions * 256
 predictions = predictions.astype(np.uint16)
+
+print("Writing predictions to", predictionFile)
+outputMidi = pretty_midi.PrettyMIDI()
+piano_program = pretty_midi.instrument_name_to_program("Acoustic Grand Piano")
+piano = pretty_midi.Instrument(program=piano_program)
+timestep = 0
+for prediction in predictions:
+    i = 0
+    for note in prediction:
+        if note > 1:
+            note = pretty_midi.Note(
+                velocity=note, pitch=i, start=timestep, end=timestep + 1
+            )
+            piano.notes.append(note)
+        i += 1
+    timestep += 1
+outputMidi.instruments.append(piano)
+outputMidi.write(predictionFile)
