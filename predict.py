52293a61f1aedea8cff87f80fc5546461067928b
Jacob Dawson <jacob.dawson@me.com>
2023-05-07 14:20:36 -0400

    add proper piano roll conversion code

--- predict.py
+++ predict.py
@@ -48,10 +48,8 @@ predictions = predictions * 256
 predictions = predictions.astype(np.uint16)
 
 print("Writing predictions to", predictionFile)
-# probably need to do more work here to get the timings correct, realizing now
-# that I don't entirely know how they were encoded.
-# TODO: determine how to turn a piano roll back into midi better
-outputMidi = pretty_midi.PrettyMIDI()
+# this is my first attempt, but it's pretty bad at reflecting how timings work
+"""outputMidi = pretty_midi.PrettyMIDI()
 piano_program = pretty_midi.instrument_name_to_program("Acoustic Grand Piano")
 piano = pretty_midi.Instrument(program=piano_program)
 timestep = 0
@@ -66,4 +64,65 @@ for prediction in predictions:
         i += 1
     timestep += 1
 outputMidi.instruments.append(piano)
+outputMidi.write(predictionFile)"""
+
+
+# here's some code I found in the pretty_midi repo that ought to wokr better
+def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
+    """Convert a Piano Roll array into a PrettyMidi object
+     with a single instrument.
+
+    Parameters
+    ----------
+    piano_roll : np.ndarray, shape=(128,frames), dtype=int
+        Piano roll of one instrument
+    fs : int
+        Sampling frequency of the columns, i.e. each column is spaced apart
+        by ``1./fs`` seconds.
+    program : int
+        The program number of the instrument.
+
+    Returns
+    -------
+    midi_object : pretty_midi.PrettyMIDI
+        A pretty_midi.PrettyMIDI class instance describing
+        the piano roll.
+
+    """
+    notes, frames = piano_roll.shape
+    pm = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(program=program)
+
+    # pad 1 column of zeros so we can acknowledge inital and ending events
+    piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], "constant")
+
+    # use changes in velocities to find note on / note off events
+    velocity_changes = np.nonzero(np.diff(piano_roll).T)
+
+    # keep track on velocities and note on times
+    prev_velocities = np.zeros(notes, dtype=int)
+    note_on_time = np.zeros(notes)
+
+    for time, note in zip(*velocity_changes):
+        # use time + 1 because of padding above
+        velocity = piano_roll[note, time + 1]
+        time = time / fs
+        if velocity > 0:
+            if prev_velocities[note] == 0:
+                note_on_time[note] = time
+                prev_velocities[note] = velocity
+        else:
+            pm_note = pretty_midi.Note(
+                velocity=prev_velocities[note],
+                pitch=note,
+                start=note_on_time[note],
+                end=time,
+            )
+            instrument.notes.append(pm_note)
+            prev_velocities[note] = 0
+    pm.instruments.append(instrument)
+    return pm
+
+
+outputMidi = piano_roll_to_pretty_midi(predictions)
 outputMidi.write(predictionFile)
